f6b0f0419b9fb82c62dff87470b1bc9181d7bf53
Matthew Beatty <beattyml1@gmail.com>
2018-07-19 18:33:56 -0400

    Remove paginator: while it wasn't letting anything proprietary out it was possibly coupled to our app by events

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,5 @@
     export * from './components/ColorPicker.vue';
     export * from './components/MultiSelect.vue';
     export * from './components/MultiSelection.vue';
-    export * from './components/Paginator.vue';
     export * from './components/ProgressBar.vue';
 </script>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,4 @@ export * from './components/Accordion.vue';
 export * from './components/ColorPicker.vue';
 export * from './components/MultiSelect.vue';
 export * from './components/MultiSelection.vue';
-export * from './components/Paginator.vue';
 export * from './components/ProgressBar.vue';
